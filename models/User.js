775e9c1de49469c60c30bd025d4c7029c412cd20
MT <69726672+M-Tri@users.noreply.github.com>
2025-07-27 18:22:29 -0600

    docs: Add comments to User model for better understanding

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../db.js'; 
 
+//It defines a database table called User. The table have two columns: name and email.
+// Sequelize automatically adds methods like findAll(), create(), and findByPk() to the model.
 export const User = sequelize.define('User', {
   name: {
     type: DataTypes.STRING,
