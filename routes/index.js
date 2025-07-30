46acc624ca7f6575c19e575212f75959ee24453f
MT <69726672+M-Tri@users.noreply.github.com>
2025-07-30 00:41:09 -0600

    feat: add ESLint for code linting and update API routes

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,10 @@ const router = express.Router();
 // When the client visits the root path (e.g., '/'), the 'home' function handles the request.
 router.get('/', home);
 
-// This means any request starting with '/api/users' will be forwarded to the routes defined in 'userRoutes.js' for further handling.
-// Each call be mount /users. Example: app.get('/api/users/', ..., This just adds the word '/users' to each call at the 'userRoutes' file.
-//api is just for clarity. It tells the client that they are using the backend.
-router.use('/api/products', productRoutes);
-router.use('/api/orders', orderRoutes);
-router.use('/api/cart', cartRoutes);
+
+// api is just for clarity. It tells the client that they are using the backend.
+router.use('/products', productRoutes);
+router.use('/orders', orderRoutes);
+router.use('/cart', cartRoutes);
 
 export default router;
