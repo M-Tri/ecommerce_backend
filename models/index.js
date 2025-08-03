bfba7742f80887291a35af9cd1abe0afa6ed4665
MT <69726672+M-Tri@users.noreply.github.com>
2025-08-02 19:42:28 -0600

    feat(cart-items): support `expand=product` query param to include product details

--- models/index.js
+++ models/index.js
@@ -4,6 +4,10 @@ import { DeliveryOption } from './DeliveryOptions.js';
 import { Order } from './Orders.js';
 import { OrderProduct} from './OrderProduct.js';
 
+// Define associations here
+CartItem.belongsTo(Product, { foreignKey: 'productId' });
+Product.hasMany(CartItem, { foreignKey: 'productId' });
+
 export {
   Product,
   CartItem,
--- routes/cartItemRoutes.js
+++ routes/cartItemRoutes.js
@@ -1,15 +1,24 @@
 import express from 'express';
-import { CartItem } from '../models/CartItem.js';
+import { CartItem, Product } from '../models/index.js';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    const cartItem = await CartItem.findAll();
-    res.json(cartItem);
+    const { expand } = req.query;
+
+    const include = [];
+    if (expand === 'product') {
+      include.push({ model: Product });
+    }
+
+    const cartItems = await CartItem.findAll({ include });
+
+    res.json(cartItems);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
 export default router;
+
